9e2911226b2bfff6ed8c476d13fe660d33aa76f4
loghmankaki <69055502+loghmankaki@users.noreply.github.com>
2022-08-03 12:49:10 +0430

    Initial Commit

---
+++ Flickr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flickr
+//
+//  Created by mac on 8/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
